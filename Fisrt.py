c584aea95e3285909c37e80fa46984e9716d54b6
Testttt <saiphone94@yandex.ru>
2025-04-17 15:22:16 +0300

    17_dosrok

--- Fisrt.py
+++ Fisrt.py
@@ -1,1 +1,8 @@
-print('Hello')
+s = [int(i) for i in open('17_21416.txt')]
+mn = sum(i for i in s if i < 0)
+res = []
+for i in range(len(s)-2):
+    x,y,z = s[i], s[i+1], s[i+2]
+    if max(x, y, z)*min(x, y, z) > mn:
+        res.append(x+y+z)
+print(len(res), max(res))
